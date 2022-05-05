d26c8c926a94a92b15c58d292e11a9a5aeeb214f
rimthekid <farbod@nigge.rs>
2022-05-05 09:34:19 -0700

    .

--- webhook.py
+++ webhook.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, Response
+from flask import Flask, request
+
 app = Flask(__name__)
-@app.route('/', methods=['POST'])
-def return_response():
-    print(request.json);
-    ## Do something with the request.json data.
-    return Response(status=200)
-if __name__ == "__main__": app.run()
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook received!"
+
+app.run(host='0.0.0.0', port=5000)
