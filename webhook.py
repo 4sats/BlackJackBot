2fff8bb6b499302a01e5b7ffbae708c01311d0e8
rimthekid <farbod@nigge.rs>
2022-05-06 02:29:50 -0700

    remove method

--- webhook.py
+++ webhook.py
@@ -2,9 +2,8 @@ from flask import Flask, request
 import config
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def webhook():
-    if request.method == 'POST':
         print("Data received from Webhook is: ", request.json)
         return "Webhook received!"
 
