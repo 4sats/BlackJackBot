ca59e9d4a3c968d84ef82d2270f50c2c5bf6c8f3
rimthekid <farbod@nigge.rs>
2022-05-17 23:31:27 -0700

    jj

--- webhook.py
+++ webhook.py
@@ -5,9 +5,9 @@ import sys
 app = Flask(__name__)
 
 @app.route('/<path:path>',methods=['POST'])
-def foo():
+def foo(path):
    #if request.remote_addr == "165.227.164.18":
-   print(request.remote_addr + str(request.json)+" fuck meeeeeeee", file=sys.stderr)
+   print(path + request.remote_addr + str(request.json)+" fuck meeeeeeee", file=sys.stderr)
    return Response(status=200)
 
 if __name__ == '__main__':
