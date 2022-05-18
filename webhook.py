10361554a71793af4f9ef628514c62f28d7216a8
rimthekid <farbod@nigge.rs>
2022-05-17 22:48:32 -0700

    kk

--- webhook.py
+++ webhook.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, Response
 import json
+import sys
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
    #data = json.loads(request.data)
-   print(str(request.data))
+   print(str(request.data), file=sys.stderr)
    return Response(status=200)
 
 if __name__ == '__main__':
