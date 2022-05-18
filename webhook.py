a5c4cdeffa65424ec9071ba363c1c8a02353fbab
rimthekid <farbod@nigge.rs>
2022-05-17 23:00:27 -0700

    log

--- webhook.py
+++ webhook.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 @app.route('/',methods=['POST'])
 def foo():
    #data = json.loads(request.data)
-   logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
    logging.debug('This: '+ str(request.data))
    return Response(status=200)
 
 if __name__ == '__main__':
+   logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
    app.run()
