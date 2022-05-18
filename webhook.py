96c5e505395c7636d88b28b230a9945c41670056
rimthekid <farbod@nigge.rs>
2022-05-17 23:07:43 -0700

    print

--- webhook.py
+++ webhook.py
@@ -1,16 +1,13 @@
 from flask import Flask, request, Response
 import json
 import sys
-import logging
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
    #data = json.loads(request.data)
-   f = open("test.txt", "a")
-   f.write("Now "+ str(request.data))
-   f.close()
+   print(str(request.data)+" fuck meeeeeeee", file=sys.stderr)
    return Response(status=200)
 
 if __name__ == '__main__':
