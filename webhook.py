00757012562f72ef77ca384d77ef66413c6c7cc9
lilcheti <farbod@tokhmi.xyz>
2022-09-04 20:58:30 -0700

    webhook fuck

--- webhook.py
+++ webhook.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def foo(path):
    if request.remote_addr == "165.227.164.18":
         print(path + request.remote_addr + str(request.json)+" fuck meeeeeeee", file=sys.stderr)
-        Database().set_balance(path,int(request.json["amount"]/1000))
+        Database().set_balance(path,int(request.json["amount"]/1000)+int(Database().get_balance(path)))
         bot = Bot(token=config.BOT_TOKEN)
         bot.send_message(chat_id=path, text="Deposited "+str(int(request.json["amount"]/1000))+"sats!")
 
